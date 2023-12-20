d7bb750625152f7c9f373ffb46227b7de529f4f2
yiyi <yiyi@xyzzdev.com>
2023-12-20 18:21:39 +0800

    交易規則樣式

--- src/views/mc/screens/TradeDetail/index.tsx
+++ src/views/mc/screens/TradeDetail/index.tsx
@@ -6,7 +6,7 @@
  * @Description:
  */
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import usePublicState from '@core/hooks/usePublicState';
 import { LS as styles } from './style';
 
@@ -21,10 +21,81 @@ export default () => {
   }, [])
 
   return (
-    <View>
-      <View>
-        <Text style={styles.title}>挂单类型123</Text>
+    <View style={styles.container}>
+      <View style={styles.dropItem}>
+        <Text>挂单类型</Text>
+        <View style={styles.dropMenu}>
+          <Text style={styles.dropText}>买入限价</Text>
+          <Image source={require('./i/ic-drop.png')} style={styles.dropIcon} />
+        </View>
       </View>
+      <View style={styles.dropItem}>
+        <Text>有效期</Text>
+        <View style={[styles.dropMenu, styles.dropWireframe]}>
+          <Text style={styles.dropText}>撤单前有效</Text>
+          <Image source={require('./i/ic-drop.png')} style={styles.dropIcon} />
+        </View>
+      </View>
+      <View style={styles.optionsItem}>
+        <Text>价格 ≤ 23.40</Text>
+        <View style={styles.optionsMenu}>
+          <Image source={require('./i/ic-reduce.png')} style={styles.optionsIcon} />
+          <Text style={styles.optionsNumber}>23.30</Text>
+          <Image source={require('./i/ic-add.png')} style={styles.optionsIcon} />
+          <Text style={styles.optionsText}>重置</Text>
+        </View>
+      </View>
+      <View style={styles.optionsItem}>
+        <Text>交易手数</Text>
+        <View style={styles.optionsMenu}>
+          <Image source={require('./i/ic-reduce.png')} style={styles.optionsIcon} />
+          <Text style={styles.optionsNumber}>0.01</Text>
+          <Image source={require('./i/ic-add.png')} style={styles.optionsIcon} />
+          <Text style={styles.optionsText}>重置</Text>
+        </View>
+      </View>
+      <View style={styles.optionsItem}>
+        <View style={[styles.frequency, styles.frequencyActive]}>
+          <Text style={styles.frequencyActiveText}>0.1手</Text>
+          <Image source={require('./i/ic-check.png')} style={styles.checkIcon} />
+        </View>
+        <View style={styles.frequency}><Text>0.3手</Text></View>
+        <View style={styles.frequency}><Text>0.5手</Text></View>
+        <View style={styles.frequency}><Text>1手</Text></View>
+        <View style={styles.frequency}><Text>2手</Text></View>
+      </View>
+      <View style={styles.dropItem}>
+        <View style={styles.refer}>
+          <Text>参考预付款：</Text>
+          <Text>9.9</Text>
+        </View>
+        <View style={styles.refer}>
+          <Text>可用预付款：</Text>
+          <Text>1000.00</Text>
+        </View>
+      </View>
+      <View style={{...styles.optionsItem, borderBottomWidth: 1,borderBottomColor: '#EBEBEB',}}>
+        <Text>止损</Text>
+        <View style={styles.optionsMenu}>
+          <Image source={require('./i/ic-reduce.png')} style={styles.optionsIcon} />
+          <Text style={styles.optionsNumber}>0.01</Text>
+          <Image source={require('./i/ic-add.png')} style={styles.optionsIcon} />
+          <Text style={styles.optionsText}>清空</Text>
+        </View>
+      </View>
+      <View style={{...styles.optionsItem, borderBottomWidth: 1,borderBottomColor: '#EBEBEB',}}>
+        <Text>止盈</Text>
+        <View style={styles.optionsMenu}>
+          <Image source={require('./i/ic-reduce.png')} style={styles.optionsIcon} />
+          <Text style={styles.optionsNumber}>未设置</Text>
+          <Image source={require('./i/ic-add.png')} style={styles.optionsIcon} />
+          <Text style={styles.optionsText}>清空</Text>
+        </View>
+      </View>
+      <View style={styles.submit}>
+        <Text style={styles.submitText}>提交</Text>
+      </View>
+      <Text style={styles.submitTips}>*市价模式下是成交价格，可能会与请求价格有一定差异</Text>
     </View>
   );
 };
--- src/views/mc/screens/TradeDetail/style.ts
+++ src/views/mc/screens/TradeDetail/style.ts
@@ -10,11 +10,133 @@ import G from '@views/mc/styles/index';
 import { StyleSheet } from 'react-native';
 
 export const LS = StyleSheet.create({
-  title:{
-    fontSize: G.mixin.rem(24),
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    fontSize: G.mixin.rem(12),
     color: '#2A2A2A',
   },
-})
+  dropItem: {
+    height: G.mixin.rem(54),
+    width: '90%',
+    flexDirection: 'row',
+    justifyContent: 'space-between', 
+    alignItems: 'center', 
+    marginHorizontal: '5%',
+    borderBottomColor: '#EBEBEB',
+    borderBottomWidth: G.mixin.rem(1),
+    fontSize: G.mixin.rem(12),
+  },
+  dropMenu: {
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    lignSelf: 'center',
+    width: G.mixin.rem(120),
+    height: G.mixin.rem(24),
+    backgroundColor: '#EBEBEB',
+    borderRadius: G.mixin.rem(50),
+  },
+  dropWireframe: {
+    backgroundColor: '#fff',
+    borderColor: '#D9D9D9',
+    borderWidth: G.mixin.rem(1),
+  },
+  dropText: {
+    flex: 1,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: '#94938F',
+  },
+  dropIcon: {
+    width: G.mixin.rem(9),
+    height: G.mixin.rem(6),
+    marginRight: G.mixin.rem(12),
+  },
+  optionsItem: {
+    height: G.mixin.rem(40),
+    width: '90%',
+    flexDirection: 'row',
+    justifyContent: 'space-between', 
+    alignItems: 'center', 
+    marginHorizontal: '5%',
+    borderBottomColor: '#EBEBEB',
+    fontSize: G.mixin.rem(12),
+    color: '#2A2A2A',
+  },
+  optionsMenu: {
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    lignSelf: 'center',
+    width: '50%',
+  },
+  optionsNumber: {
+    color: '#94938F',
+    flex: 1,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    width:'30%',
+  },
+  optionsText: {
+    color: '#94938F',
+    marginLeft: G.mixin.rem(20),
+  },
+  optionsIcon: {
+    width: G.mixin.rem(15),
+    height: G.mixin.rem(20),
+  },
+  refer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  frequency: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: G.mixin.rem(24),
+    marginLeft: G.mixin.rem(5),
+    borderRadius: G.mixin.rem(3),
+    borderColor: '#AAAAAA',
+    borderWidth: G.mixin.rem(1),
+    fontSize: G.mixin.rem(10),
+  },
+  frequencyActive: {
+    backgroundColor: '#2A2A2A',
+    borderWidth: G.mixin.rem(0),
+    marginLeft: G.mixin.rem(0),
+  },
+  frequencyActiveText: {
+    color: '#E7CC8F',
+    fontWeight: 'bold',
+  },
+  checkIcon: {
+    width: G.mixin.rem(15),
+    height: G.mixin.rem(15),
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+  },
+  submit: {
+    width: '80%',
+    height: G.mixin.rem(40),
+    backgroundColor: '#2A2A2A',
+    borderRadius: G.mixin.rem(50),
+    marginHorizontal: '10%',
+    marginTop: G.mixin.rem(20),
+    marginBottom: G.mixin.rem(20),
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  submitText: {
+    color: '#E7CC8F',
+    fontSize: G.mixin.rem(15),
+  },
+  submitTips: {
+    color: '#94938F',
+    fontSize: G.mixin.rem(12),
+    textAlign: 'center',
+    marginBottom: G.mixin.rem(20),
+  },
+});
 
 // 固定导出
 export const GS = G;
