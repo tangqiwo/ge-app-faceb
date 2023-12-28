0c9645359c96bde6145f4b20ed0e88510bb63075
yiyi <yiyi@xyzzdev.com>
2023-12-28 14:11:50 +0800

    市價

--- src/views/mc/screens/TradeDetail/Done/index.tsx
+++ src/views/mc/screens/TradeDetail/Done/index.tsx
@@ -7,7 +7,8 @@
  */
 import _ from 'lodash';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import MyTouchableOpacity from '@core/templates/components/MyTouchableOpacity';
 import { LS as styles } from './style';
 import usePublicState from '@core/hooks/usePublicState';
 
@@ -24,9 +25,62 @@ export default () => {
 
   return (
     <View style={styles.container}>
-      <Text>
-        DONE !
-      </Text>
+      <Image source={require('./i/market.png')} style={styles.image}/>
+      <View style={styles.main}>
+        <View style={styles.date}>
+          <Text style={styles.dateText}>2023-10-30 23:22:38 GMT+0800</Text>
+          <Text style={styles.dateText}> (中国标准时间)</Text>
+        </View>
+        <View style={styles.content}>
+          <View style={styles.left}>
+            <View style={styles.item}>
+              <Text style={styles.grey}>单号： </Text>
+              <Text>10015</Text>
+            </View>
+            <View style={styles.item}>
+              <Text style={styles.grey}>价格： </Text>
+              <Text>10015</Text>
+            </View>
+            <View style={styles.item}>
+              <Text style={styles.grey}>方向： </Text>
+              <Text>卖出</Text>
+            </View>
+            <View style={styles.item}>
+              <Text style={styles.grey}>止盈： </Text>
+              <Text>10015</Text>
+            </View>
+          </View>
+          <View style={styles.right}>
+            <View style={styles.item}>
+              <Text style={styles.grey}>类型： </Text>
+              <Text>平仓</Text>
+            </View>
+            <View style={styles.item}>
+              <Text style={styles.grey}>产品： </Text>
+              <Text>XAUUSDpro</Text>
+            </View>
+            <View style={styles.item}>
+              <Text style={styles.grey}>手数： </Text>
+              <View style={styles.item}>
+                <Text>0.01</Text>
+                <Text>手</Text>
+              </View>
+            </View>
+            <View style={styles.item}>
+              <Text style={styles.grey}>止损： </Text>
+              <Text>10015</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+      <View style={styles.button}>
+        <MyTouchableOpacity style={styles.buttonBlack}>
+          <Text style={styles.buttonTextYellow}>查看持仓</Text>
+        </MyTouchableOpacity>
+        <MyTouchableOpacity style={styles.buttonYellow}>
+          <Text style={styles.buttonText}>继续交易</Text>
+        </MyTouchableOpacity>
+      </View> 
     </View>
   );
 };
--- src/views/mc/screens/TradeDetail/Done/style.ts
+++ src/views/mc/screens/TradeDetail/Done/style.ts
@@ -15,8 +15,88 @@ export const LS = StyleSheet.create({
     backgroundColor: '#fff',
     fontSize: G.mixin.rem(12),
     color: '#2A2A2A',
+    padding: G.mixin.rem(16),
   },
-});
+  image: {
+    width:'100%',
+    height:G.mixin.rem(100),
+    resizeMode:'contain',
+  },
+  main: {
+    backgroundColor: '#F2F2F2',
+    borderBottomLeftRadius:G.mixin.rem(5),
+    borderBottomRightRadius:G.mixin.rem(5),
+    width: '100%',
+    height: 'auto',
+  },
+  date: {
+    backgroundColor:'#2A2A2A',
+    paddingHorizontal:G.mixin.rem(5),
+    paddingVertical:G.mixin.rem(5),
+    borderTopRightRadius:G.mixin.rem(20),
+    borderBottomRightRadius:G.mixin.rem(20),
+    marginTop:G.mixin.rem(16),
+    flexDirection: 'row',
+    width: G.mixin.rem(248),
+  },
+  dateText: {
+    color: '#E7CC8F',
+    fontSize: G.mixin.rem(10),
+  },
+  content: {
+    padding: G.mixin.rem(16),
+    paddingBottom: G.mixin.rem(0),
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  left: {
+    width: '50%',
+  },
+  right: {
+    width: '50%',
+  },
+  item: {
+    flexDirection: 'row',
+    height: G.mixin.rem(32),
+  },
+  grey: {
+    color: '#94938F',
+  },
+  button: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    width: '100%',
+    marginTop: G.mixin.rem(30),
+  },
+  buttonYellow: {
+    height: G.mixin.rem(40),
+    width: G.mixin.rem(160),
+    backgroundColor: '#FFC600',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: G.mixin.rem(20),
+  },
+  buttonBlack: {
+    height: G.mixin.rem(40),
+    width: G.mixin.rem(160),
+    backgroundColor: '#2A2A2A',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: G.mixin.rem(30),
+    color: '#fff',
+  },
+  buttonText : {
+    fontSize: G.mixin.rem(15),
+  },
+  buttonTextYellow : {
+    fontSize: G.mixin.rem(15),
+    color: '#E7CC8F',
+  },
+
+  }
+);
 
 // 固定导出
 export const GS = G;
